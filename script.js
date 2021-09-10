445e67cfcc62f8432e2d7109604c5d1ca9c10d69
somexhey <behhokc@gmail.com>
2021-09-10 14:52:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-<script type="text/javascript" language="javascript">
     function RunFile() {
     WshShell = new ActiveXObject("WScript.Shell");
     WshShell.Run("c:/windows/system32/notepad.exe", 1, false);
     }
-</script>
